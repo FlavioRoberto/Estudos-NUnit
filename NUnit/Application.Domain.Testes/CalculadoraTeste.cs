186ed6aebda1e1ebd0c373caf2157ba07809d62e
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-11-08 12:04:51 -0200

    Initial Commmit

---
+++ NUnit/Application.Domain.Testes/CalculadoraTeste.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Domain.Testes
+{
+    [TestFixture]
+    public class CalculadoraTeste
+    {
+        [Test]
+        public void SomarDoisNumeros()
+        {
+            var calc = new Calculadora();
+            var result = calc.Somar(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void MultiplicarDoisNumeros()
+        {
+
+            var calc = new Calculadora();
+            var result = calc.Multiplicar(2, 10);
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void SubtrairDoisNumeros()
+        {
+            var calc = new Calculadora();
+            var result = calc.Subtrair(10,5);
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
---
+++ NUnit/Application.Domain/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Application.Domain
+{
+    public class Calculadora
+    {
+
+        public double Somar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public object Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
